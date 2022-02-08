bad008d1def3f085f8bcada4d613477f5c825eab
Fergasi <fergusmagor@gmail.com>
2022-02-07 22:51:20 -0500

    add

---
+++ weekday.js
@@ -0,0 +1,29 @@
+const prompt = require('prompt-sync')();
+
+let num1 = Number(prompt('Enter a number: '));
+
+
+if (num1 === 1) {
+    //do this
+    console.log('Monday');
+} else if (num1 === 2) {
+    //do this
+    console.log('Tuesday');
+} else if (num1 === 3) {
+    //do this
+    console.log('Wednesday');
+} else if (num1 === 4) {
+    //do this
+    console.log('Thursday');
+} else if (num1 === 5) {
+    //do this
+    console.log('Friday');
+} else if (num1 === 6) {
+    //do this
+    console.log('Saturday');
+} else if (num1 === 7) {
+    //do this
+    console.log('Sunday');
+} else {
+    console.log('error');
+}
